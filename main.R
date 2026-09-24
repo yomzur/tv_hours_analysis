08b790285763134451d690cc0e60a7c9e4d5b7aa
yomzur <68952889+yomzur@users.noreply.github.com>
2026-09-24 16:00:25 +0100

    shorten names

--- main.R
+++ main.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 library(here)
 
-tv_hours_table <- gss_cat %>% 
+tv_hours_tab <- gss_cat %>% 
   filter(age < 30) %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hours = mean(tvhours, na.rm = TRUE))
 
-write_csv(tv_hours_table, here("TV_hours_by_Marital.csv"))
+write_csv(tv_hours_tab, here("TV_hours_by_Marital.csv"))
